4170d82e56b9edb0b5fd2abf84f4a24a0524286e
chisato <elmalinao20@gmail.com>
2024-04-05 07:37:29 +0800

    update

---
+++ back.sh
@@ -0,0 +1,11 @@
+rm ~/terraria/worlds/ROUND2.twld
+rm ~/terraria/worlds/ROUND2.twld.bak
+rm ~/terraria/worlds/ROUND2.wld
+rm ~/terraria/worlds/ROUND2.wld.bak
+rm ~/terraria/worlds/ROUND2.wld.bak2
+
+cp ~/.local/share/Terraria/tModLoader/Worlds/ROUND2.twld ~/terraria/worlds/
+cp ~/.local/share/Terraria/tModLoader/Worlds/ROUND2.twld.bak ~/terraria/worlds/
+cp ~/.local/share/Terraria/tModLoader/Worlds/ROUND2.wld ~/terraria/worlds/
+cp ~/.local/share/Terraria/tModLoader/Worlds/ROUND2.wld.bak ~/terraria/worlds/
+cp ~/.local/share/Terraria/tModLoader/Worlds/ROUND2.wld.bak2 ~/terraria/worlds/
