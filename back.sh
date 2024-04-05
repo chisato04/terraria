d17ca8a525fc4119095fd9774a97098eceab5481
chisato <elmalinao20@gmail.com>
2024-04-05 21:49:29 +0800

    delete

--- back.sh
+++ back.sh
@@ -1,5 +1,1 @@
-cp ~/.local/share/Terraria/tModLoader/Worlds/ROUND2.twld ~/terraria/worlds/
-cp ~/.local/share/Terraria/tModLoader/Worlds/ROUND2.twld.bak ~/terraria/worlds/
-cp ~/.local/share/Terraria/tModLoader/Worlds/ROUND2.wld ~/terraria/worlds/
-cp ~/.local/share/Terraria/tModLoader/Worlds/ROUND2.wld.bak ~/terraria/worlds/
-cp ~/.local/share/Terraria/tModLoader/Worlds/ROUND2.wld.bak2 ~/terraria/worlds/
+cp -r ~/.local/share/Terraria/tModLoader/Worlds ~/terraria/worlds
